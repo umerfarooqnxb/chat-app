6ee9a250fce8f5a74588d9c1db2fe3b4ed49bd37
umerfarooqnxb <umer.farooq2885@nxb.com.pk>
2023-05-26 12:01:52 +0500

    Implement Socket Server with websocket

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const http = require('http')
+const socketio = require('socket.io')
 const app = express()
 require('dotenv').config()
+
+const server = http.createServer(app)
+// Socket IO instance is to expect to be called with raw http request 
+const io = socketio(server)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + `/public/index.html`)
 })
 
+io.on('connection',()=>{
+    console.log("Web socket connection")
+})
 
-app.listen(process.env.PORT, () => console.log(`Server is listening on ${process.env.PORT} port`))
+
+server.listen(process.env.PORT, () => console.log(`Server is listening on ${process.env.PORT} port`))
